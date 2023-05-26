d25e2e59005bfab9b3060ee72021cf726bfee6c0
AkynBayram <akinbayram56@gmail.com>
2023-05-26 20:35:57 +0300

    Genre

--- books/models.py
+++ books/models.py
@@ -5,3 +5,12 @@ class Book(models.Model):
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     year = models.IntegerField()
+
+    def __str__(self):
+        return f"Книги"
+
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
+
